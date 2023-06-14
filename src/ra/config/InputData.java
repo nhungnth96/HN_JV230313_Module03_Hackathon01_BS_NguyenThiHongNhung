14b54679c5b0906295c7a1c55616ed7d08f6c92a
Nhung Nguyen <128070316+nhungnth96@users.noreply.github.com>
2023-06-14 11:45:26 +0700

    Delete src/ra/config directory

--- src/ra/config/InputData.java
+++
@@ -1,9 +0,0 @@
-package ra.config;
-
-import java.util.Scanner;
-
-public class InputData {
-    public static Scanner scanner(){
-        return new Scanner(System.in);
-    }
-}
--- src/ra/config/Validate.java
+++
@@ -1,4 +0,0 @@
-package ra.config;
-
-public class Validate {
-}
